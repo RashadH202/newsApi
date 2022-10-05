6c3b07f228fbc5a09638b53bf07dc65daa6d89ec
Raashad Hammonds <114700867+RashadH202@users.noreply.github.com>
2022-10-05 14:34:56 -0400

    Initial commit

---
+++ client/frontend/README.md
@@ -0,0 +1,7 @@
+# React API Template
+
+I created this template to have a base for my application..
+
+# Technology 
+
+Monogoose, Expresss, React, Node, Materialize CSS
---
+++ client/frontend/src/Componets/Display/Display.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './display.css'
+import { Collection, CollectionItem, Col, Row } from 'react-materialize'
+const Display = () => {
+
+
+
+  return (
+    <div className='display__data'>
+      
+      <Row>
+    <Col
+      m={12}
+      s={12}
+    >
+      <Collection>
+        <CollectionItem href="#">
+          Alvin
+        </CollectionItem>
+
+          
+      </Collection>
+    </Col>
+  </Row></div>
+  )
+}
+
+export default Display
